4e63bcca32eef6bbe02a725a774e2e3cf4d396d0
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-04-10 12:38:27 +0200

    Updates

---
+++ src/js/ui/footer.js
@@ -0,0 +1,75 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const footer = document.querySelector(".footer-custom")
+  footer.innerHTML = ` <div class="container-fluid bg-dark text-white vh-100r">
+  <div class="row p-3 gap-4">
+    <div
+      class="col-sm d-flex justify-content-center align-content-center flex-column"
+    >
+      <h5>Auction on Bidme</h5>
+      <span>Upcoming auctions</span>
+      <span>Finished auctions</span>
+      <span>Partners</span>
+      <span>Artists</span>
+    </div>
+    <div
+      class="col-sm d-flex justify-content-center align-content-center flex-column"
+    >
+      <h5>Let us help you</h5>
+      <span>Contact</span>
+      <span>Terms and conditions</span>
+      <span>Cookies</span>
+      <span>Privacy Policy</span>
+    </div>
+    <div class="col-sm d-flex align-content-center flex-column">
+      <h5 class="mb-3">Follow us</h5>
+      <div class="d-flex justify-content-start align-content-center gap-4">
+        <i
+          class="fa-brands fa-facebook text-dark p-2 bg-white rounded-circle"
+        ></i>
+        <i
+          class="fa-brands fa-square-instagram text-dark p-2 bg-white rounded-circle"
+        ></i>
+        <i
+          class="fa-brands fa-linkedin-in text-dark p-2 bg-white rounded-circle"
+        ></i>
+      </div>
+    </div>
+  </div>
+  <div class="d-flex justify-content-start align-items-center">
+    <div
+      class="row bg-dark d-flex align-content-center justify-content-center p-3"
+    >
+      <div
+        class="col d-flex align-content-center justify-content-center flex-column gap-2"
+      >
+        <h5>Be up to date</h5>
+        <span
+          >Subscribe to the free newsletter, you'll find out about current
+          auctions and interesting lots.</span
+        >
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Email address"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+          />
+          <div class="input-group-append">
+            <button class="btn btn-primary text-white" type="button">
+              Button
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container-fluid custom-height-pre-navbar bg-light p-1">
+  <div class="d-flex justify-content-center align-items-center">
+    <h6>&copy; 2024 Weronika Martinsen. All rights reserved.</h6>
+  </div>
+</div>
+  
+  `
+})
---
+++ src/js/ui/navbar.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar-custom")
+  navbar.innerHTML = `
+    <div class="container-fluid custom-height-pre-navbar bg-dark"></div>
+    <header class="bg-light sticky-top">
+      <nav class="navbar navbar-expand-sm text-white">
+        <div class="container-fluid d-flex align-items-center">
+        <button
+        class="navbar-toggler custom-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="fa-solid fa-bars"></span>
+      </button>
+      <div class="float-left">
+        <a class="navbar-brand" href="listings.html">
+          <img
+            class="custom-logo-size-feed"
+            src="/images/logo-bidme.png"
+            alt="Logo of the website - Bidme."
+          />
+        </a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="listings.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Auctions</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              <img
+                class="custom-avatar-size"
+                src="/images/avatar-bidme.png"
+                alt="Logo of the website - Bidme."
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+        </div>
+      </nav>
+    </header>
+  `
+})
