cc35967c64a8c3bafc502805473baa38ee3a751b
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-04-11 20:31:04 +0200

    Adds html validation

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -31,7 +31,7 @@ export async function login(user) {
         userEmail: json.email,
       })
 
-      window.location.href = "feed/index.html"
+      window.location.href = "html/listings/listings.html"
     } else {
       const status = json.statusCode
       if (status === 401) {
