4c2cbf897ae0fe5ef9b870a9bd33de952f7d6f7c
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-04-19 12:31:46 +0200

    Working on listings cars

--- src/js/listings/listingCard.js
+++ src/js/listings/listingCard.js
@@ -29,7 +29,6 @@ function formatEndDateTime(date) {
 }
 
 export function createListingCard(listing) {
-  console.log(listing)
   const card = document.createElement("div")
   card.classList.add("listing-card")
 
@@ -39,43 +38,59 @@ export function createListingCard(listing) {
 
   const image = document.createElement("img")
   if (listing.media && listing.media.length > 0) {
-    // Display the first image if available
     image.src = listing.media[0]
   } else {
-    // Set default image source if no images are available
-    image.src = "/images/defaultImage.png" // Path to your default image
+    image.src = "/images/defaultImage.png"
   }
   image.alt = "Listing Image"
+  image.onerror = function () {
+    this.src = "/images/defaultImage.png"
+  }
   imageContainer.appendChild(image)
 
   card.appendChild(imageContainer)
 
-  const contentContainer = document.createElement("div")
-  contentContainer.classList.add("contentContainer")
-
-  card.appendChild(contentContainer)
-
   const sellerContainer = document.createElement("div")
   sellerContainer.classList.add("sellerContainer")
+
+  // Append sellerContainer to card
+  card.appendChild(sellerContainer)
+
   const avatarImg = document.createElement("img")
   avatarImg.src = listing.seller.avatar || "/images/avatar-bidme.png"
   avatarImg.alt = "Avatar"
   avatarImg.classList.add("sellerAvatar")
 
-  const sellerName = document.createElement("a")
-  sellerName.textContent = listing.seller.name
-
-  contentContainer.appendChild(sellerContainer)
+  // Append avatarImg to sellerContainer
   sellerContainer.appendChild(avatarImg)
-  sellerContainer.appendChild(sellerName)
+
+  const avatarAndDate = document.createElement("div")
+  avatarAndDate.classList.add("avatarAndDate")
+
+  // Append avatarAndDate to sellerContainer
+  sellerContainer.appendChild(avatarAndDate)
 
   // Other details
+
+  const sellerName = document.createElement("a")
+  sellerName.textContent = listing.seller.name
+  sellerName.classList.add("h6", "text-dark")
+
+  // Append sellerName to avatarAndDate
+  avatarAndDate.appendChild(sellerName)
 
   const details = document.createElement("div")
   details.classList.add("details")
   const createdDate = new Date(listing.created)
   const formattedDate = formatDateTime(createdDate)
   details.textContent = `${formattedDate}`
+
+  // Append details to avatarAndDate
+  avatarAndDate.appendChild(details)
+
+  const contentContainer = document.createElement("div")
+  contentContainer.classList.add("contentContainer")
+  card.appendChild(contentContainer)
 
   const auctionsEnd = document.createElement("div")
   auctionsEnd.classList.add("auctionEnds")
@@ -93,11 +108,10 @@ export function createListingCard(listing) {
   auctionsEnd.appendChild(timeElement)
   auctionsEnd.appendChild(dateElement)
 
-  contentContainer.appendChild(details)
-
   // Title
-  const title = document.createElement("h2")
+  const title = document.createElement("h6")
   title.textContent = listing.title
+  title.classList.add("mt-1")
   contentContainer.appendChild(title)
 
   // Description
@@ -117,6 +131,53 @@ export function createListingCard(listing) {
     })
     contentContainer.appendChild(tagsContainer)
   }
+
+  // Bids
+  const bidsCount = listing._count.bids || 0 // Get the count of bids
+  const bidsContainer = document.createElement("div")
+  bidsContainer.classList.add("bids-container")
+
+  if (bidsCount > 0) {
+    const lastBid = listing.bids[listing.bids.length - 1] // Get the last bid
+
+    const bidsCountElement = document.createElement("p")
+    bidsCountElement.textContent = `Bids: ${bidsCount}`
+    bidsContainer.appendChild(bidsCountElement)
+
+    contentContainer.appendChild(bidsContainer)
+
+    const lastBidContainer = document.createElement("div")
+    lastBidContainer.classList.add("last-bid-container")
+
+    const lastBidderName = document.createElement("a")
+    const lastBidAmount = document.createElement("div")
+    lastBidAmount.classList.add("d-flex", "gap-1")
+    lastBidAmount.innerHTML = `<h2 class="text-primary">${lastBid.amount},-</h2><span class="small-font-size">bidder:</span><a class="bidder small-font-size">${lastBid.bidderName}</a>`
+
+    lastBidContainer.appendChild(lastBidAmount)
+    lastBidContainer.appendChild(lastBidderName)
+
+    bidsContainer.appendChild(lastBidContainer)
+  }
+
+  // SEE MORE (link to ID)
+
+  const seeMore = document.createElement("a")
+  seeMore.textContent = "SEE MORE"
+  seeMore.classList.add("seeMore")
+
+  card.appendChild(seeMore)
+
+  const btnContainer = document.createElement("div")
+  btnContainer.classList.add("btnContainer")
+
+  const bidBtn = document.createElement("button")
+  bidBtn.textContent = "BID"
+  bidBtn.classList.add("btn", "btn-primary", "text-white")
+
+  btnContainer.appendChild(bidBtn)
+
+  card.appendChild(btnContainer)
 
   return card
 }
