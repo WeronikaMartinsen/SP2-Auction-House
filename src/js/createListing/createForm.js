000cbc8a1516aec5992068dbb3289dad8a47de60
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-04-23 14:02:08 +0200

    Assigned value to the modal for creating listing

--- src/js/createListing/createForm.js
+++ src/js/createListing/createForm.js
@@ -9,15 +9,19 @@ export function createNewListing() {
       getForm.addEventListener("sumbit", (event) => {
         event.preventDefault()
         const form = event.target
-
-        const title = form.title.value
-        const description = form.description.value
-        const media = form.media.value
-
+        const title = form.querySelector("#title").value
+        const description = form.querySelector("#description").value
+        const media1 = form.querySelector("#media1").value
+        const media2 = form.querySelector("#media2").value
+        const media3 = form.querySelector("#media3").value
+        const deadline = form.querySelector("#deadline").value
         const newListing = {
           title,
           description,
-          media,
+          media1,
+          media2,
+          media3,
+          deadline,
         }
         createListing(newListing)
         userFeedback("Your post has been added!", () => {
