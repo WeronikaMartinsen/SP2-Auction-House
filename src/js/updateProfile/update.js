9ad594568c90091c53004dc2e397859436725199
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-05-10 22:40:48 +0200

    working on update profile

---
+++ src/js/updateProfile/update.js
@@ -0,0 +1,46 @@
+import { API_BASE_URL, PROFILES, API_KEY_NAME } from "../api/constants.js"
+import { load } from "../api/storage/storeToken.js"
+import { handleError } from "../userFeedback/errorMessage.js"
+import { userFeedback } from "../userFeedback/feedbackOverlay.js"
+
+export async function updateProfile(editedProfile) {
+  const updateProfileURL = `${API_BASE_URL}${PROFILES}/${user}`
+  const token = load("token")
+  const getProfileFromToken = load("profile")
+  const user = getProfileFromToken.userName
+
+  try {
+    const postData = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY_NAME,
+      },
+      body: JSON.stringify(editedProfile),
+    }
+
+    const response = await fetch(updateProfileURL, postData)
+
+    if (response.ok) {
+      userFeedback("You have successfully updated your profile.", () => {
+        window.location.href = `/html/profiles/profile.html?name=${user}`
+      })
+    } else {
+      // Handle error cases
+      console.error("Error:", response.statusText)
+      handleError("Error updating listing. Please try again.")
+      userFeedback("Something went wrong. Please, try again.", () => {
+        // Callback function to execute after the timeout
+        location.reload()
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    handleError("Error updating listing. Please try again.")
+    userFeedback("Something went wrong. Please, try again.", () => {
+      // Callback function to execute after the timeout
+      location.reload()
+    })
+  }
+}
---
+++ src/js/updateProfile/updateProfileForm.js
@@ -0,0 +1,49 @@
+import { update } from "./update.js"
+import { load } from "../api/storage/storeToken"
+import { toggleAvatarForm } from "./toggleAvatar"
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const profileMediaButton = document.getElementById("profileMedia")
+  const toggleAvatarFormButton = document.getElementById("toggleAvatarForm")
+
+  if (profileMediaButton && toggleAvatarFormButton) {
+    const getProfileFromToken = load("profile")
+    const user = getProfileFromToken.userName
+    window.location.href = `/html/profiles/profile.html?name=${user}`
+
+    if (user) {
+      toggleAvatarFormButton.style.display = "inline-block"
+      toggleAvatarForm()
+    } else {
+      toggleAvatarFormButton.style.display = "none"
+    }
+
+    profileMediaButton.addEventListener("click", async function (event) {
+      event.preventDefault()
+
+      const avatarInput = document.getElementById("avatarInput")
+      const avatarImage = document.getElementById("avatar")
+      const avatarImageNav = document.getElementById("avatar-nav")
+
+      const newAvatarUrl = avatarInput.value
+
+      if (user) {
+        if (newAvatarUrl) {
+          avatarImage.src = newAvatarUrl
+          avatarImageNav.src = newAvatarUrl
+
+          try {
+            await update(newAvatarUrl, user)
+            location.reload()
+          } catch (error) {
+            console.error("Error updating avatar:", error)
+          }
+        } else {
+          console.error("Avatar URL is required.")
+        }
+      } else {
+        console.error("You can only update your own avatar.")
+      }
+    })
+  }
+})
