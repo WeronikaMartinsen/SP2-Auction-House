fdc9342e545f28bb0c2211338eeaa3eb462109bd
WeronikaMartinsen <121826608+WeronikaMartinsen@users.noreply.github.com>
2024-05-15 06:04:45 +0200

    working on navbar

--- src/js/ui/navbar.js
+++ src/js/ui/navbar.js
@@ -6,54 +6,63 @@ document.addEventListener("DOMContentLoaded", function () {
   const navbar = document.querySelector(".navbar-custom")
 
   navbar.innerHTML = `
-  <div class="container-fluid custom-height-pre-navbar bg-dark d-flex justify-content-end align-items-center">
-  </div>
-  <nav class="navbar bg-light">
-  <div class="navbar-logo">
-    <a href="#" id="profile-logo">
-      <img src="/images/logo-bidme.png" alt="Logo of the website - Bidme." class="custom-logo-size-feed">
-    </a>
-  </div>
-  <div class="navbar-toggler">
-  <i class="fa-solid fa-bars"></i>
-  </div>
-  <div class="navbar-additional-content bg-light" id="navbarAdditionalContent">
-    <div class="navbar-content-item d-flex flex-column">
-      <a href="#" id="name-navbar" class="text-primary text-end h5 p-0"></a>
-      <div class="d-flex gap-1"><i class="fa-solid fa-coins"></i><span id="credits-navbar" class="text-dark text-end small-font-size p-0"></span></div>
+    <div class="container-fluid custom-height-pre-navbar bg-dark d-flex justify-content-end align-items-center">
     </div>
-    <div class="navbar-content-item">
-      <a href="#" id="avatar-small-navbar">
-        <img src="/images/avatar-bidme.png" alt="Avatar." id="avatar-navbar" class="custom-avatar-size avatar-img shadow">
-      </a>
-    </div>
-    <div class="navbar-content-item custom-padding-logout-btn">
-      <a href="#" id="logout" class="btn btn-dark rounded-4 text-white px-4 shadow-sm">Logout</a>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar bg-light justify-content-between align-items-center">
+      <div class="navbar-logo">
+        <a href="#" id="profile-logo">
+          <img src="/images/logo-bidme.png" alt="Logo of the website - Bidme." class="custom-logo-size-feed">
+        </a>
+      </div>
+      <div class="navbar-additional-content bg-light d-flex justify-content-end align-items-end" id="navbarAdditionalContent">
+        <div class="navbar-content-item">
+          <a href="#" class="nav-link">Listings</a>
+        </div>
+        <div class="navbar-content-item">
+          <a href="#" class="nav-link">Profile</a>
+        </div>
+        <div class="navbar-content-item">
+          <a href="#" class="nav-link">Sell Listing</a>
+        </div>
+        <div class="navbar-content-item custom-padding-logout-btn">
+        <a href="#" id="logout" class="text-dark text-end small-font-size p-0"><i class="fa-solid fa-right-from-bracket text-dark"></i> Logout</a>
+      </div>
+        <div class="navbar-content-item d-flex flex-column">
+          <a href="#" id="name-navbar" class="text-primary text-end h5 p-0"></a>
+          <div class="d-flex gap-1">
+            <i class="fa-solid fa-coins"></i>
+            <span id="credits-navbar" class="text-dark text-end small-font-size p-0"></span>
+          </div>
+        </div>
+        <div class="navbar-content-item m-2">
+          <a href="#" id="avatar-small-navbar">
+            <img src="/images/avatar-bidme.png" alt="Avatar." id="avatar-navbar" class="custom-avatar-size avatar-img shadow">
+          </a>
+        </div>
+        
+      </div>
+      <div class="navbar-toggler-custome">
+        <i class="fa-solid fa-bars"></i>
+      </div>
+     
+    </nav>
   `
-  const navbarToggle = document.querySelector(".navbar-toggler")
+
+  const navbarToggle = document.querySelector(".navbar-toggler-custome")
+  const navbarAdditionalContent = document.getElementById(
+    "navbarAdditionalContent",
+  )
+
   navbarToggle.addEventListener("click", () => {
-    const navbarAdditionalContent = document.querySelector(
-      "#navbarAdditionalContent",
-    )
-    if (navbarAdditionalContent.style.display === "block") {
-      navbarAdditionalContent.style.display = "none"
-    } else {
-      navbarAdditionalContent.style.display = "block"
-    }
+    navbarAdditionalContent.classList.toggle("show")
   })
+
   function resetNavbarDisplay() {
-    const navbarAdditionalContent = document.querySelector(
-      "#navbarAdditionalContent",
-    )
-    if (window.innerWidth > 500) {
-      navbarAdditionalContent.style.display = "flex"
-    } else {
-      navbarAdditionalContent.style.display = "none"
+    if (window.innerWidth > 600) {
+      navbarAdditionalContent.classList.remove("show")
     }
   }
+
   resetNavbarDisplay()
 
   // Add event listener for window resize
